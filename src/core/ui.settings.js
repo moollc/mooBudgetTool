62386e3236e11dfce9ec311f99e0fe2551c9e75f
moollc <moo@moollc.com>
2026-07-18 14:41:42 -0500

    app cache refreshes so phones and desktops pick up the latest build

--- src/core/ui.settings.js
+++ src/core/ui.settings.js
@@ -749,7 +749,7 @@
             var updateStatus = (window.mBT && window.mBT.registry && window.mBT.registry.updateStatus) || {};
             var updateAvailable = updateStatus.available || false;
             var isChecking = updateStatus.checking || false;
-            var currentVersion = updateStatus.localVersion || 'v23.91';
+            var currentVersion = updateStatus.localVersion || 'v23.92';
 
             var statusMsg = '';
             if (isChecking) {
